de2ebd7649bab6d1c51bcf9300feac4fd4aa56f5
Sufiyan <StarKhan6368@gmail.com>
2025-10-16 14:47:11 +0530

    Add testContainers setup

---
+++ src/__test__/testContainer.test.js
@@ -0,0 +1,57 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import { GenericContainer, Wait } from "testcontainers";
+import 'core-js'
+import path from "node:path";
+import App from '../App'
+const jp = require('jsonpath')
+
+/**
+ * @type {import("testcontainers").StartedTestContainer}
+ */
+let stub
+const GADGET_LIST_EXPECTATION_FILE = "specmatic-expectations/gadget_list.json"
+const expectationsDir = path.resolve("src/__test__/specmatic-expectations")
+const stubHost = "localhost"
+const stubPort = 9000
+const timeout = 1500
+
+beforeAll(async () => {
+    process.env.REACT_APP_API_URL = `http://${stubHost}:${stubPort}`
+    stub = await new GenericContainer("specmatic/specmatic")
+        .withBindMounts([
+            { source: path.resolve("specmatic.yaml"), target: "/usr/src/app/specmatic.yaml" },
+            { source: expectationsDir, target: "/usr/src/app/examples" }
+        ])
+        .withCommand(["virtualize", "--examples", "examples", "--port", `${stubPort}`])
+        .withExposedPorts({ host: stubPort, container: stubPort })
+        .withLogConsumer(stream => {
+            stream.on("data", line => console.log(line));
+            stream.on("err", line => console.error(line));
+            stream.on("end", () => console.log("Specmatic log stream ended"));
+        })
+        .withWaitStrategy(Wait.forLogMessage(/Stub server is running/i))
+        .start();
+}, 120000)
+
+test('renders gadgets list', async () => {
+    render(<App type="gadget" />)
+    const gadgetListExpectationObject = require(`./${GADGET_LIST_EXPECTATION_FILE}`)
+    const gadgetList = jp.query(gadgetListExpectationObject, '$..body[*]')
+    await waitFor(() => expect(screen.getAllByText("Product name").length).toBe(gadgetList.length), { timeout })
+    for (const gadget of gadgetList) expect(screen.getByText(gadget.name)).toBeInTheDocument()
+}, 120000)
+
+test('Empty Product List', async () => {
+    render(<App type="headphone" />)
+    await waitFor(() => expect(screen.getByText("No Products found")).toBeInTheDocument(), { timeout })
+}, 120000)
+
+
+test('timeout error', async () => {
+    render(<App type="bike" />)
+    await waitFor(() => expect(screen.getByText("Timeout! Please try again...")).toBeInTheDocument(), { timeout })
+}, 120000)
+
+afterAll(async () => {
+    await stub.stop()
+}, 120000)
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+import { TextEncoder, TextDecoder } from 'util';
+import { ReadableStream, WritableStream } from 'stream/web';
+import { MessageChannel, MessagePort } from 'worker_threads';
+Object.assign(global, { TextDecoder, TextEncoder });
+Object.assign(global, { ReadableStream, WritableStream });
+Object.assign(global, { MessageChannel, MessagePort });
