644c4ab42b66587bcbd42cc222c099d2788ff8dc
jaydeep <jaydeepk@gmail.com>
2025-12-17 13:03:22 +0530

    Trying with just  one test running

--- src/__test__/testContainer.test.js
+++ src/__test__/testContainer.test.js
@@ -41,16 +41,16 @@ test('renders gadgets list', async () => {
     for (const gadget of gadgetList) expect(screen.getByText(gadget.name)).toBeInTheDocument()
 }, 120000)
 
-test('Empty Product List', async () => {
-    render(<App type="headphone" />)
-    await waitFor(() => expect(screen.getByText("No Products found")).toBeInTheDocument(), { timeout })
-}, 120000)
+// test('Empty Product List', async () => {
+//     render(<App type="headphone" />)
+//     await waitFor(() => expect(screen.getByText("No Products found")).toBeInTheDocument(), { timeout })
+// }, 120000)
 
 
-test('timeout error', async () => {
-    render(<App type="bike" />)
-    await waitFor(() => expect(screen.getByText("Timeout! Please try again...")).toBeInTheDocument(), { timeout })
-}, 120000)
+// test('timeout error', async () => {
+//     render(<App type="bike" />)
+//     await waitFor(() => expect(screen.getByText("Timeout! Please try again...")).toBeInTheDocument(), { timeout })
+// }, 120000)
 
 afterAll(async () => {
     await stub.stop()
